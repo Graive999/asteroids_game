d08decb8a67272a23b228a54974094250f20c130
graive999 <tdh679@gmail.com>
2026-01-31 17:20:02 -0500

    Add collisions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -39,6 +40,12 @@ def main():
 
 # updating all updatables?
         updatable.update(dt) 
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         
 # drawings
         screen.fill("black")
