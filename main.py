4bc79c783df3df96e5f5aa14ea48db260f3208d2
graive999 <tdh679@gmail.com>
2026-01-31 19:10:14 -0500

    Add killing asteroids

--- main.py
+++ main.py
@@ -50,6 +50,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         
 # drawings
         screen.fill("black")
